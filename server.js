42812c4f740c7d2fcc4a273a9f0b01c9e88e7bad
Genat Hadri <gh43836@ubt-uni.net>
2021-07-22 17:21:26 +0200

    first commit

--- server.js
+++ server.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
+//Connect Database
+connectDB();
+
+app.use(express.json({ extended: false}));
+
 app.get('/', (req, res) => res.send('API Running '));
+
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
 
 const PORT = process.env.PORT || 5000;
 

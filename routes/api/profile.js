02845ed54719c5f387576fe1f1da19201d4ad61e
Genat Hadri <gh43836@ubt-uni.net>
2021-07-26 18:13:03 +0200

    delete profile and user

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -97,7 +97,7 @@ router.post(
                 //Create
                 profile = new Profile(profileFields);   
                 
-                profile.save();
+                await profile.save();
                 res.json(profile); 
         }catch(err){
             console.error(err.message);
@@ -112,7 +112,6 @@ router.get('/', async(req, res) => {
     try {
         const profiles = await Profile.find().populate('user', ['name', 'avatar']);
         res.json(profiles);
-        console.log('done');
     } catch (err) {
         console.error(err.message);
         res.status(500).send('Server Error');
@@ -135,4 +134,24 @@ router.get('/user/:user_id', async(req, res) => {
         res.status(500).send('Server Error');
     }
 });
+
+
+// @route   DELETE api/profile
+// @desc    Delete profile, user & posts
+// @access  Private
+router.delete('/', auth, async(req, res) => {
+    try {
+        // @todo - Remove users posts
+
+        // Remove profile
+        await Profile.findOneAndRemove({ user: req.user.id });
+        // Remove user
+        await User.findOneAndRemove({ _id: req.user.id });
+        res.json({ msg: 'User deleted' });
+    } catch (err) {
+        console.error(err.message);
+        res.status(500).send('Server Error');
+    }
+});
+
 module.exports = router;

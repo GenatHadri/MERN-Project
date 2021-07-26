fbbc208bc23082fd365201724ecc46ef2fb0be58
Genat Hadri <gh43836@ubt-uni.net>
2021-07-26 23:30:27 +0200

    delete profile experience

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -168,7 +168,7 @@ async(req, res) => {
     if(!errors.isEmpty()){
         return res.status(400).json({ errors: errors.array() });
     } 
-    
+
     const {
         title,
         company,
@@ -201,6 +201,26 @@ async(req, res) => {
        res.status(500).send('Server Error');  
     }
 
+});
+
+// @route   DELETE api/profile/experience/:exp_id
+// @desc    Delete experience from profile
+// @access  Private
+router.delete('/experience/:exp_id', auth, async(req, res) => {
+    try{
+        const profile = await Profile.findOne({ user: req.user.id }); 
+
+        // Get remove index
+        const removeIndex = profile.experience.map(item => item.id).indexOf(req.params.exp_id);
+
+        profile.experience.splice(removeIndex, 1);
+
+        await profile.save();
+        res.json(profile);
+    }catch(err){
+        console.error(err.message);
+        res.status(500).send('Server Error');  
+    }
 });
 
 module.exports = router;
